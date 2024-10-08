5126605f34b3f2c44f723418839175cd3533e670
JT <41375007h@gapps.ntnu.edu.tw>
2024-10-09 04:17:08 +0800

    add sh file

---
+++ HW1/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir output
+cd output
+# for mc (lert-base)
+gdown --folder https://drive.google.com/drive/folders/1ZLUauVin9gdKfxK5pOVLFl4gnZa9gLia
+# for qa (lert-large)
+godwn --folder https://drive.google.com/drive/folders/1SCZ24XsLSEIGydgflb8qRPxNJHSlWUnB
---
+++ HW1/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python src/inference_mc.py \
+    --test_file "${2}" \
+    --context_file "${1}" \
+    --model_name_or_path output/chinese-lert-base/mc \
+    --tokenizer_name output/chinese-lert-base/mc \
+    --pad_to_max_length \
+    --max_seq_length 512 \
+    --per_device_eval_batch_size 100 \
+    --output_dir output/chinese-lert-base/mc                            
+
+python src/inference_qa.py  \
+    --test_file output/chinese-lert-base/mc/test.json \
+    --context_file data/context.json \
+    --model_name_or_path output/chinese-lert-large/qa \
+    --tokenizer_name output/chinese-lert-large/qa \
+    --pad_to_max_length --max_seq_length 512 \
+    --per_device_eval_batch_size 100 \
+    --output_dir "${3}"
