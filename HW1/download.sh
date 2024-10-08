4ac88dc38b54257706d55cddb218426fa0b87afc
JT <41375007h@gapps.ntnu.edu.tw>
2024-10-09 04:28:15 +0800

    modify download.sh

--- HW1/download.sh
+++ HW1/download.sh
@@ -4,4 +4,4 @@ cd output
 # for mc (lert-base)
 gdown --folder https://drive.google.com/drive/folders/1ZLUauVin9gdKfxK5pOVLFl4gnZa9gLia
 # for qa (lert-large)
-godwn --folder https://drive.google.com/drive/folders/1SCZ24XsLSEIGydgflb8qRPxNJHSlWUnB
+gdown --folder https://drive.google.com/drive/folders/1SCZ24XsLSEIGydgflb8qRPxNJHSlWUnB
