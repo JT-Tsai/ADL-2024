403c52b2531aff3796a8cabf06c43a6be981113a
JT <41375007h@gapps.ntnu.edu.tw>
2024-10-15 01:25:10 +0800

    src modify 3

--- HW2/src/summarization.py
+++ HW2/src/summarization.py
@@ -426,20 +426,20 @@ def main():
         
         accelerator.print(f"Resumed from checkpoint: {checkpoint_path}")
         accelerator.load_state(checkpoint_path)
-        
-    # Extract  `epoch_{i}` or `step_{i}`
-    training_difference = os.path.splitext(path)[0]
+            
+        # Extract  `epoch_{i}` or `step_{i}`
+        training_difference = os.path.splitext(path)[0]
 
-    if "epoch" in training_difference:
-        starting_epoch = int(training_difference.replace("epoch_", "")) + 1
-        resume_step = None
-        completed_steps = starting_epoch * num_update_steps_per_epoch
-    else:
-        # need to multiply `gradient_accumulation_steps` to reflect real steps
-        resume_step = int(training_difference.replace("step_", "")) * args.gradient_accumulation_steps
-        starting_epoch = resume_step // len(train_dataloader)
-        completed_steps = resume_step // args.gradient_accumulation_steps
-        resume_step -= starting_epoch * len(train_dataloader)
+        if "epoch" in training_difference:
+            starting_epoch = int(training_difference.replace("epoch_", "")) + 1
+            resume_step = None
+            completed_steps = starting_epoch * num_update_steps_per_epoch
+        else:
+            # need to multiply `gradient_accumulation_steps` to reflect real steps
+            resume_step = int(training_difference.replace("step_", "")) * args.gradient_accumulation_steps
+            starting_epoch = resume_step // len(train_dataloader)
+            completed_steps = resume_step // args.gradient_accumulation_steps
+            resume_step -= starting_epoch * len(train_dataloader)
 
     # update the progress_bar if load from checkpoint
     progress_bar.update(completed_steps)
