78f36256e147f37f55006ccb8aed41c2e5880ff3
JT <41375007h@gapps.ntnu.edu.tw>
2024-10-24 12:36:16 +0800

    infer_testing

--- HW2/infer_test.sh
+++ HW2/infer_test.sh
@@ -4,7 +4,7 @@ python src/inference.py \
     --jsonl_data_file data/data/public.jsonl \
     --model_name_or_path output/first_train \
     --pad_to_max_length \
-    --per_device_eval_batch_size 16 \
+    --per_device_eval_batch_size 64 \
     --val_max_target_length 128 \
     --num_beams 4 \
     --do_sample \
