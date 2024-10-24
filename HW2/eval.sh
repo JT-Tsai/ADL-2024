a62b74c6d6e3c917e04d9cf8c2d037453a1026d8
JT <41375007h@gapps.ntnu.edu.tw>
2024-10-24 22:12:28 +0800

    infer_testing

---
+++ HW2/eval.sh
@@ -0,0 +1,72 @@
+#!bin/bash
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/1.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/2.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/3.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/4.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/5.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/6.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/7.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/8.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/9.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/10.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/11.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/12.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/13.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/14.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/15.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/16.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/17.jsonl
+
+python src/eval.py \
+    -r data/data/public.jsonl \
+    -s result/tmp.jsonl
